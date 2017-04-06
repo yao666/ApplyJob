449f40838cb5f348f00269a8087b1be29bd78454
Tom <Tom@PC>
2017-04-06 18:27:10 +0800

    求职首页导航完成

--- ApplyJob/App_Start/RouteConfig.cs
+++ ApplyJob/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ApplyJob
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Personal", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
